d152dbf12a8d494e4e77616bb539534e9a6c8b94
ToasterUwU <Aki@ToasterUwU.com>
2024-02-28 23:42:03 +0100

    change the plasma config a bit

--- common/plasma.nix
+++ common/plasma.nix
@@ -16,24 +16,14 @@
       programs.plasma = {
         workspace = {
           clickItemTo = "select";
-          tooltipDelay = 5;
           theme = "breeze-dark";
           colorScheme = "BreezeDark";
+          iconTheme = "Papirus-Dark";
         };
 
         kwin.titlebarButtons = {
           left = [ "on-all-desktops" "keep-above-windows" ];
-          right = [ "help" "minimize" "maximize" "close" ];
-        };
-
-        spectacle.shortcuts = {
-          captureActiveWindow = "Meta+Print";
-          captureCurrentMonitor = "Print";
-          captureEntireDesktop = "Shift+Print";
-          captureRectangularRegion = "Meta+Shift+S";
-          captureWindowUnderCursor = "Meta+Ctrl+Print";
-          launch = "Meta+S";
-          launchWithoutCapturing = "Meta+Alt+S";
+          right = [ "minimize" "maximize" "close" ];
         };
       };
     };
