be81cf470311bcbe4da892c6442aab9b95f97770
ToasterUwU <aki@toasteruwu.com>
2024-03-01 14:34:35 +0100

    git config

--- common/home-manager.nix
+++ common/home-manager.nix
@@ -12,6 +12,11 @@
       xdg.configFile."hyfetch.json".text = ''
         {"preset": "transgender", "mode": "rgb", "light_dark": "dark", "lightness": 0.65, "color_align": {"mode": "horizontal", "custom_colors": [], "fore_back": null}, "backend": "neofetch", "distro": null, "pride_month_shown": []}
       '';
+      programs.git = {
+        enable = true;
+        userName  = "ToasterUwU";
+        userEmail = "Aki@ToasterUwU.com";
+      };
     };
   };
 }
