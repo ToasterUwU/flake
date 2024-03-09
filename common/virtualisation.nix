3567d4fc69a4b47cd0971496e8ac7d0dec988337
ToasterUwU <Aki@ToasterUwU.com>
2024-03-09 18:23:21 +0100

    add waydroid

--- common/virtualisation.nix
+++ common/virtualisation.nix
@@ -5,4 +5,6 @@
   virtualisation.virtualbox.host.enable = true;
   virtualisation.virtualbox.host.enableExtensionPack = true;
   users.extraGroups.vboxusers.members = [ "aki" ];
+
+  virtualisation.waydroid.enable = true;
 }
