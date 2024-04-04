d5ca8023a89290b201d71d7e2a8ec1b24d3dc2ba
ToasterUwU <Aki@ToasterUwU.com>
2024-04-04 23:04:07 +0200

    add virtualbox guest additions

--- common/virtualisation.nix
+++ common/virtualisation.nix
@@ -4,6 +4,7 @@
 
   virtualisation.virtualbox.host.enable = true;
   virtualisation.virtualbox.host.enableExtensionPack = true;
+  virtualisation.virtualbox.guest.enable = true;
   users.extraGroups.vboxusers.members = [ "aki" ];
 
   virtualisation.waydroid.enable = true;
