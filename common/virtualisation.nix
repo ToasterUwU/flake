b6629b624a8818a4c5c00a9e422c6c441711218b
ToasterUwU <Aki@ToasterUwU.com>
2024-04-04 23:29:09 +0200

    The option is for vms, not for hosts

--- common/virtualisation.nix
+++ common/virtualisation.nix
@@ -4,7 +4,6 @@
 
   virtualisation.virtualbox.host.enable = true;
   virtualisation.virtualbox.host.enableExtensionPack = true;
-  virtualisation.virtualbox.guest.enable = true;
   users.extraGroups.vboxusers.members = [ "aki" ];
 
   virtualisation.waydroid.enable = true;
