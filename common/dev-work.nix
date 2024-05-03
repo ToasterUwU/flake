94997563d4c3b8958c0eb177cc788a2fbba406ba
ToasterUwU <Aki@ToasterUwU.com>
2024-05-03 15:20:22 +0200

    Use VSCode FHS

--- common/dev-work.nix
+++ common/dev-work.nix
@@ -1,14 +1,28 @@
-{ pkgs, ... }: {
-  imports = [ ];
+{ inputs, pkgs, ... }: {
+  imports = [ inputs.home-manager.nixosModules.home-manager ];
+
+  home-manager = {
+    users.aki = {
+      programs.vscode = {
+        enable = true;
+        package = pkgs.vscode.fhsWithPackages (ps: with ps; [
+          fira-code
+          nil
+          zlib
+          gcc
+          pkg-config
+          rustup
+          openssl.dev
+          systemd.dev
+        ]);
+      };
+    };
+  };
 
   users.users.aki = {
     packages = with pkgs; [
-      fira-code
-      vscode
-      nil
       nixpkgs-fmt
       python3
-      rustup
       nodejs
     ];
   };
