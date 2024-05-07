d8c81da08440d2c17ba9cabdc3219c3d940bfe78
ToasterUwU <Aki@ToasterUwU.com>
2024-05-07 23:08:22 +0200

    tealdeer config for auto update

--- common/home-manager.nix
+++ common/home-manager.nix
@@ -10,29 +10,38 @@
 
     users.aki = {
       home.stateVersion = "23.11";
-      programs.hyfetch = {
-        enable = true;
-        settings = {
-          preset = "transgender";
-          mode = "rgb";
-          light_dark = "dark";
-          lightness = 0.65;
-          color_align = {
-            mode = "horizontal";
-            custom_colors = [ ];
-            fore_back = null;
+      programs = {
+        hyfetch = {
+          enable = true;
+          settings = {
+            preset = "transgender";
+            mode = "rgb";
+            light_dark = "dark";
+            lightness = 0.65;
+            color_align = {
+              mode = "horizontal";
+              custom_colors = [ ];
+              fore_back = null;
+            };
+            backend = "fastfetch";
+            distro = null;
+            pride_month_shown = [ ];
           };
-          backend = "fastfetch";
-          distro = null;
-          pride_month_shown = [ ];
         };
-      };
-      programs.bash = {
-        enable = true;
-        bashrcExtra = ''
-        eval "$(zoxide init --cmd cd bash)"
-        hyfetch
-        '';
+        bash = {
+          enable = true;
+          bashrcExtra = ''
+            eval "$(zoxide init --cmd cd bash)"
+            hyfetch
+          '';
+        };
+        tealdeer = {
+          enable = true;
+          settings.updates = {
+            auto_update = true;
+            auto_update_interval_hours = 24;
+          };
+        };
       };
       xdg.configFile."autostart/vesktop.desktop".text = ''
         [Desktop Entry]
