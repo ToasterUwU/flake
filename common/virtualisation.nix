5ac85a791c8b0550fe33fa1ad63c04c70a8ad743
ToasterUwU <Aki@ToasterUwU.com>
2024-05-08 12:50:07 +0200

    exclude my old backup laptop from the long building process of virtualbox

--- common/virtualisation.nix
+++ common/virtualisation.nix
@@ -1,10 +1,14 @@
-{ ... }: {
+{ config, ... }:
+let
+  isWaltraud = config.networking.hostName == "Waltraud";
+in
+{
   virtualisation.docker.enable = true;
   users.extraGroups.docker.members = [ "aki" ];
 
-  virtualisation.virtualbox.host.enable = true;
-  virtualisation.virtualbox.host.enableExtensionPack = true;
-  users.extraGroups.vboxusers.members = [ "aki" ];
+  virtualisation.virtualbox.host.enable = !isWaltraud;
+  virtualisation.virtualbox.host.enableExtensionPack = !isWaltraud;
+  users.extraGroups.vboxusers.members = if isWaltraud then [] else [ "aki" ];
 
   virtualisation.waydroid.enable = true;
 }
