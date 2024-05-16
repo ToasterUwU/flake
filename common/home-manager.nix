3476d1b8a7cd57af0fc0076a5948274eb496c256
ToasterUwU <Aki@ToasterUwU.com>
2024-05-16 11:11:06 +0200

    add update vm script and desktop file

--- common/home-manager.nix
+++ common/home-manager.nix
@@ -94,6 +94,52 @@
         StartupNotify=false
         Terminal=false
       '';
+      xdg.desktopEntries."Update VMs" = {
+        type = "Application";
+        name = "Update VMs";
+        exec = "bash /home/aki/update_vms.sh";
+        icon = "system-software-update";
+        terminal = true;
+      };
+      home.file."update_vms.sh" = {
+        executable = true;
+        text = ''
+          #!/bin/bash
+
+          HOSTS_TO_UPDATE="airvpn-tunnel-vm
+          discord-bots
+          mongo-db
+          ollama
+          smart-home
+          video-station
+          xen-orchestra"
+
+          # Get Password
+          read -sp "Password please: " PASSWORD
+          echo ""
+
+          # Getting sudo perms
+          echo $PASSWORD | sudo -S echo "Thanks, checking password" >/dev/null 2>&1 # -S just yeets the password into sudo so i dont have to type it out again, and also dont need to remember to use sudo on the script
+          echo ""
+
+          sudo -n true 2>/dev/null
+          if ! [ $? -eq 0 ]; then
+              echo "Password Wrong"
+              read -p "Press enter to continue"
+              exit 0
+          fi
+
+          for host in $HOSTS_TO_UPDATE; do
+              echo -e "\033[1m\033[32m$host\033[0m"
+              sshpass -p $PASSWORD ssh $host "echo $PASSWORD | DEBIAN_FRONTEND=noninteractive sudo -S bash update.sh"
+              echo -e "\033[1m\033[32mDone\033[0m"
+              echo ""
+          done
+
+          echo "Updated everything, VMs are rebooting"
+          read -p "Press enter to continue"
+        '';
+      };
     };
     users.scarlett = {
       home.stateVersion = "23.11";
