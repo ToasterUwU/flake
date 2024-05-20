2a107b8024ba6cc0683cdc395e409c9ba64b2d6c
ToasterUwU <Aki@ToasterUwU.com>
2024-05-20 20:59:43 +0200

    fix cursors for scarlett

--- common/plasma.nix
+++ common/plasma.nix
@@ -194,7 +194,10 @@
           right = [ "minimize" "maximize" "close" ];
         };
 
-        configFile.kcminputrc.Keyboard.NumLock.value = 0;
+        configFile.kcminputrc = {
+          Keyboard.NumLock.value = 0;
+          Mouse.cursorTheme = "Catppuccin-Mocha-Pink-Cursors";
+        };
         configFile.kxkbrc.Layout = {
           LayoutList = "gb";
           Use = true;
