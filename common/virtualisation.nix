32b0a003c0518f6086b1d8c946e4fc89f751ee6f
ToasterUwU <Aki@ToasterUwU.com>
2024-05-25 00:16:34 +0200

    Not yet compatible with kernel 6.9, see NixOS/nixpkgs#312336

--- common/virtualisation.nix
+++ common/virtualisation.nix
@@ -6,8 +6,8 @@ in
   virtualisation.docker.enable = true;
   users.extraGroups.docker.members = [ "aki" ];
 
-  virtualisation.virtualbox.host.enable = !isWaltraud;
-  virtualisation.virtualbox.host.enableExtensionPack = !isWaltraud;
+  virtualisation.virtualbox.host.enable = false; # !isWaltraud;
+  virtualisation.virtualbox.host.enableExtensionPack = false; # !isWaltraud;
   users.extraGroups.vboxusers.members = if isWaltraud then [ ] else [ "aki" ];
 
   virtualisation.waydroid.enable = true;
