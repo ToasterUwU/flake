def8ede7521f43bf443761a49a4d516dad800154
ToasterUwU <Aki@ToasterUwU.com>
2024-07-25 00:49:53 +0200

    use new integrated config option for no sleep on charging

--- common/plasma.nix
+++ common/plasma.nix
@@ -87,7 +87,8 @@
         configFile.kcminputrc.Keyboard.NumLock.value = 0;
         configFile.kwinrc.Effect-overview.BorderActivate = 9;
         configFile.kwinrc.EdgeBarrier.EdgeBarrier = 0;
-        configFile.powerdevilrc."AC/SuspendAndShutdown".AutoSuspendAction = 0; # When charging or when Desktop, no sleep
+
+        powerdevil.autoSuspend.action = "nothing"; # When charging or when Desktop, no sleep
 
         panels = [
           {
