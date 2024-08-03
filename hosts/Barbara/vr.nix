9b9182d9e23b4bf894404a34a3f146016688b19f
ToasterUwU <Aki@ToasterUwU.com>
2024-08-03 17:01:45 +0200

    formating

--- hosts/Barbara/vr.nix
+++ hosts/Barbara/vr.nix
@@ -5,16 +5,14 @@
     inputs.home-manager.nixosModules.home-manager
   ];
 
-  boot.kernelPatches = [
-  {
+  boot.kernelPatches = [{
     name = "amdgpu-ignore-ctx-privileges";
     patch = pkgs.fetchpatch {
       name = "cap_sys_nice_begone.patch";
       url = "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
       hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
     };
-  }
-];
+  }];
 
   programs.envision.enable = true;
   environment.systemPackages = with pkgs; [
