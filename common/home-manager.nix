38c4aefadb7b48cb14cb26708b4f526ad3f29158
ToasterUwU <Aki@ToasterUwU.com>
2024-08-03 23:07:09 +0200

    delete another backup file that is in the way

--- common/home-manager.nix
+++ common/home-manager.nix
@@ -13,6 +13,12 @@
               rm -f "$FILE_PATH"
               echo "File $FILE_PATH has been deleted."
             fi
+
+            FILE_PATH="$dir/.config/gtk-4.0/gtk.css.backup"
+            if [ -e "$FILE_PATH" ]; then
+              rm -f "$FILE_PATH"
+              echo "File $FILE_PATH has been deleted."
+            fi
           fi
         done
       '';
