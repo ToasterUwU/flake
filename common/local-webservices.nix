42139d084706d1c4cfc999c93b22cb8694cb6a53
ToasterUwU <Aki@ToasterUwU.com>
2024-08-05 11:51:54 +0200

    reenable fixed ollama

--- common/local-webservices.nix
+++ common/local-webservices.nix
@@ -1,10 +1,10 @@
 { ... }: {
   imports = [ ];
 
-  # services.ollama = {
-  #   enable = true;
-  #   host = "localhost";
-  # };
+  services.ollama = {
+    enable = true;
+    host = "localhost";
+  };
 
   # services.open-webui.enable = true;
 }
