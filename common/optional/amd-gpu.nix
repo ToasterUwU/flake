4391e5fcf2c888243d61747730b326e4ee2a2aff
ToasterUwU <Aki@ToasterUwU.com>
2024-10-12 16:18:56 +0200

    add opencl support

--- common/optional/amd-gpu.nix
+++ common/optional/amd-gpu.nix
@@ -1,8 +1,10 @@
-{ ... }: {
+{ pkgs, ... }: {
   imports = [ ];
 
   boot.initrd.kernelModules = [ "amdgpu" ];
   services.xserver.videoDrivers = [ "amdgpu" ];
+
+  hardware.graphics.extraPackages = with pkgs; [ rocmPackages.clr.icd ];
 
   nixpkgs.config.rocmSupport = true;
 }
