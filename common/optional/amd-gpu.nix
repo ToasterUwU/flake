0e6a2270e7a26d5103f618121911e060e4b829cf
ToasterUwU <Aki@ToasterUwU.com>
2025-02-15 13:50:18 +0100

    fix rocm ollama amd build failure for rocm webui

--- common/optional/amd-gpu.nix
+++ common/optional/amd-gpu.nix
@@ -6,5 +6,12 @@
 
   hardware.graphics.extraPackages = with pkgs; [ rocmPackages.clr.icd ];
 
-  nixpkgs.config.rocmSupport = true;
+  # nixpkgs.config.rocmSupport = true;
+  services.ollama = {
+    acceleration = "rocm";
+    environmentVariables = {
+      HCC_AMDGPU_TARGET = "gfx1100"; # used to be necessary, but doesn't seem to anymore
+    };
+    rocmOverrideGfx = "11.0.0";
+  };
 }
