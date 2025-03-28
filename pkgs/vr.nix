342c5e0d9da44e2a9bfa09d73328d5e6842dc15d
ToasterUwU <Aki@ToasterUwU.com>
2025-03-28 22:39:23 +0100

    Fix desktop entries for monado start and stop scripts

--- pkgs/vr.nix
+++ pkgs/vr.nix
@@ -1,36 +1,37 @@
 { pkgs, ... }:
 let
+  monado-start-desktop = pkgs.makeDesktopItem {
+    exec = "monado-start";
+    icon = "steamvr";
+    name = "Start Monado";
+    desktopName = "Start Monado";
+    terminal = true;
+  };
+
+  monado-stop-desktop = pkgs.makeDesktopItem {
+    exec = "monado-stop";
+    icon = "steamvr";
+    name = "Stop Monado";
+    desktopName = "Stop Monado";
+    terminal = true;
+  };
+
   monado-run-scripts = pkgs.stdenv.mkDerivation {
     pname = "monado-run-scripts";
     version = "1.1";
 
     src = ../assets/scripts/monado-run-scripts;
 
-    desktopItems = [
-      (pkgs.makeDesktopItem {
-        exec = "monado-start";
-        icon = "steamvr";
-        name = "Start Monado";
-        desktopName = "Start Monado";
-        terminal = true;
-      })
-      (pkgs.makeDesktopItem {
-        exec = "monado-stop";
-        icon = "steamvr";
-        name = "Stop Monado";
-        desktopName = "Stop Monado";
-        terminal = true;
-      })
-    ];
-
     buildInputs = [ pkgs.bash ];
 
     installPhase = ''
-      mkdir -p $out/bin
+      mkdir -p $out/bin $out/share/applications
       cp $src/start.sh $out/bin/monado-start
       chmod +x $out/bin/monado-start
       cp $src/stop.sh $out/bin/monado-stop
       chmod +x $out/bin/monado-stop
+      cp -r ${monado-start-desktop}/* $out/
+      cp -r ${monado-stop-desktop}/* $out/
     '';
 
     meta = with pkgs.lib; {
