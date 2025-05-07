1790ba3058495f480af5a2897d5e1ee5a2b5d599
ToasterUwU <Aki@ToasterUwU.com>
2025-05-07 11:12:32 +0200

    more info about modes, and remove the pacing comp time thing since i dont know that i actually need it

--- hosts/Barbara/vr.nix
+++ hosts/Barbara/vr.nix
@@ -29,13 +29,16 @@
     STEAMVR_LH_ENABLE = "true";
     XRT_COMPOSITOR_COMPUTE = "1";
     XRT_COMPOSITOR_SCALE_PERCENTAGE = "200";
-    XRT_COMPOSITOR_DESIRED_MODE = "2"; # Valve Index: 0=90hz 1=80hz 2=120hz 3=144hz (default=4)
-    U_PACING_COMP_MIN_TIME_MS = "3";
+    XRT_COMPOSITOR_DESIRED_MODE = "2";
+    # 0: 2880x1600@90.00 1: 2880x1600@144.00 2: 2880x1600@120.02 3: 2880x1600@80.00 4: 1920x1200@90.00
+    # 5: 1920x1080@90.00 6: 1600x1200@90.00 7: 1680x1050@90.00 8: 1280x1024@90.00 9: 1440x900@90.00
+    # 10: 1280x800@90.00 11: 1280x720@90.00 12: 1024x768@90.00 13: 800x600@90.00 14: 640x480@90.00
   };
 
   home-manager = {
     users.aki = {
-      xdg.configFile."openxr/1/active_runtime.json".source = "${pkgs.monado}/share/openxr/1/openxr_monado.json";
+      xdg.configFile."openxr/1/active_runtime.json".source =
+        "${pkgs.monado}/share/openxr/1/openxr_monado.json";
       xdg.configFile."openvr/openvrpaths.vrpath".text = ''
         {
           "config" :
