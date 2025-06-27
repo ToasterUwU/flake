39f76de01aa45d540d20a784a942da110b8568fe
ToasterUwU <Aki@ToasterUwU.com>
2025-06-27 10:14:30 +0200

    fix ledger live by unsetting the wayland env var for electron

--- common/configuration.nix
+++ common/configuration.nix
@@ -186,7 +186,10 @@
       gearlever
       # openscad-unstable
       orca-slicer
-      ledger-live-desktop
+      (pkgs.writeShellScriptBin "ledger-live-desktop" ''
+        # unset NIXOS_OZONE_WL for this app only
+        exec env -u NIXOS_OZONE_WL ${pkgs.ledger-live-desktop}/bin/ledger-live-desktop "$@"
+      '')
       monero-gui
       chromedriver
       intiface-central
