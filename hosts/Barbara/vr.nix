65b60f3b40350cbfb1e3a8b272a4c7ac22606e0e
ToasterUwU <Aki@ToasterUwU.com>
2025-06-29 11:09:01 +0200

    disable kernel patch, since i dont use steamvr currently, i will leave it so i can easily reenable it

--- hosts/Barbara/vr.nix
+++ hosts/Barbara/vr.nix
@@ -10,16 +10,17 @@
     ../../pkgs/vr.nix
   ];
 
-  boot.kernelPatches = [
-    {
-      name = "amdgpu-ignore-ctx-privileges";
-      patch = pkgs.fetchpatch {
-        name = "cap_sys_nice_begone.patch";
-        url = "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
-        hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
-      };
-    }
-  ];
+  # SteamVR async reprojection patch
+  # boot.kernelPatches = [
+  #   {
+  #     name = "amdgpu-ignore-ctx-privileges";
+  #     patch = pkgs.fetchpatch {
+  #       name = "cap_sys_nice_begone.patch";
+  #       url = "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
+  #       hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
+  #     };
+  #   }
+  # ];
 
   programs.steam = {
     extraCompatPackages = with pkgs; [ proton-ge-rtsp-bin ];
