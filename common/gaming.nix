b9a32065c9de3f6d2b4c98a87cba13c2a8b4c654
ToasterUwU <Aki@ToasterUwU.com>
2025-09-18 23:11:34 +0200

    disable fix for mirrors edge catalyst since it breaks online play for burnout paradise

--- common/gaming.nix
+++ common/gaming.nix
@@ -45,7 +45,7 @@
   ];
 
   networking.hosts = {
-    "127.0.0.1" = [ "winter15.gosredirector.ea.com" ]; # A fix for "Mirrors Edge Catalyst". Without this it will try to ping a server that has been shutdown, then fail and crash
+    # "127.0.0.1" = [ "winter15.gosredirector.ea.com" ]; # A fix for "Mirrors Edge Catalyst". Without this it will try to ping a server that has been shutdown, then fail and crash
   };
 
   home-manager = {
