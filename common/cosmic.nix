f68c92801ef988d87b33f5eab151010c60525e60
ToasterUwU <Aki@ToasterUwU.com>
2025-10-29 03:32:54 +0100

    disable cosmic for now, it likes to get in the way when using something else

--- common/cosmic.nix
+++ common/cosmic.nix
@@ -4,5 +4,5 @@
   # services.displayManager.cosmic-greeter.enable = true;
 
   # Enable the COSMIC desktop environment
-  services.desktopManager.cosmic.enable = true;
+  # services.desktopManager.cosmic.enable = true;
 }
