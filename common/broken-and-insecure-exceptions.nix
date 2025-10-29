82e32af57f8f377553ea1c6e083518cbf4eb533c
ToasterUwU <Aki@ToasterUwU.com>
2025-10-29 03:33:59 +0100

    disallow insecure packages again, since only one was still needed and now isnt anymore either

--- common/broken-and-insecure-exceptions.nix
+++ common/broken-and-insecure-exceptions.nix
@@ -3,15 +3,5 @@
   imports = [ ];
 
   nixpkgs.config.permittedInsecurePackages = [
-    "dotnet-runtime-wrapped-7.0.20"
-    "dotnet-runtime-7.0.20"
-    "dotnet-core-combined"
-    "dotnet-sdk-7.0.410"
-    "dotnet-sdk-6.0.428"
-    "dotnet-sdk-wrapped-7.0.410"
-    "electron-33.4.11"
-    "libsoup-2.74.3"
-    "libxml2-2.13.8"
-    "qtwebengine-5.15.19"
   ];
 }
