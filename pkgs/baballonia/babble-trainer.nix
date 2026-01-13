d5d110cd0b700ed2349abfcb96eae05041bfecd6
ToasterUwU <Aki@ToasterUwU.com>
2026-01-13 01:15:43 +0100

    Update Baballonia Package, builds and works fine, but is certainly janky. I will clean this up another day when i feel like it

--- pkgs/baballonia/babble-trainer.nix
+++ pkgs/baballonia/babble-trainer.nix
@@ -1,8 +1,7 @@
 {
-  config,
   pkgs,
   lib,
-  enableCuda ? config.cudaSupport,
+  enableCuda,
   ...
 }:
 let
@@ -10,7 +9,7 @@ let
 
   onnxscript = pyPkgs.buildPythonPackage rec {
     pname = "onnxscript";
-    version = "0.5.6";
+    version = "0.5.7";
     format = "wheel";
     src = pkgs.fetchPypi {
       inherit pname version format;
@@ -18,13 +17,13 @@ let
       dist = "py3";
       abi = "none";
       platform = "any";
-      hash = "sha256-sMM1X+o+7KuMopHai3ev3cqs062l7lkpQ5CgSeoSOTg=";
+      hash = "sha256-+UpmBZxW0TtEkI6bf9na5LT6pmgceE8/1MKc+oY+RU4=";
     };
   };
 
   onnx-ir = pyPkgs.buildPythonPackage rec {
     pname = "onnx-ir";
-    version = "0.1.12";
+    version = "0.1.14";
     format = "wheel";
     src = pkgs.fetchPypi {
       inherit version format;
@@ -33,69 +32,90 @@ let
       dist = "py3";
       abi = "none";
       platform = "any";
-      hash = "sha256-F/hvr4pTuXlDC94bxgIsehYrDRU0VQ3bF6HTfrmT52U=";
+      hash = "sha256-ibIS+nhAmBxdtdxHgZDxtzaVNil8PG6uaPscIjfdJVQ=";
     };
+  };
+
+  babble-trainer-src = pkgs.fetchFromGitHub {
+    owner = "Project-Babble";
+    repo = "BabbleTrainer";
+    rev = "1.3.8-linux-paths";
+    hash = "sha256-ECK4ApmPl2X41w8yshjUTdZleR4X3Tmh47r0CO28f0Y=";
+  };
+
+  opencv2 = pkgs.opencv.override (old: {
+    enableGtk2 = true;
+  });
+
+  babble-data = pyPkgs.buildPythonPackage {
+    pname = "babble_data";
+    version = "1.3.8-linux-paths";
+    src = "${babble-trainer-src}/babble_data";
+    pyproject = true;
+    build-system = [ pyPkgs.setuptools ];
+    buildInputs = [
+      pyPkgs.numpy
+      opencv2
+    ] ++ lib.optionals enableCuda [ pkgs.cudaPackages.cudatoolkit ];
+    nativeBuildInputs = [ pkgs.pkg-config ];
   };
 
   # Python depedencies we should have. For some reason,
   # nixpkgs do not include all ONNX support packages so
   # we have to fetch them ourselves.
   pythonEnv = pkgs.python3.withPackages (ps: [
-    (if enableCuda then ps.torchWithCuda else ps.torch)
+    ps.torch
     # Direct depedencies:
     ps.numpy
     ps.onnx
     ps.opencv-python
     ps.pillow
+    ps.tqdm
     # Indrect depedency for ONNX exporting:
     ps.torchvision
     # Out-of-tree depedencies for ONNX exporting:
     onnxscript
     onnx-ir
+    babble-data
   ]);
 in
 pyPkgs.buildPythonPackage {
-  # TODO: figure out how this works on ROCm...
-  # * It should just be the same package because of HIP.
-  # * Need to verify and test.
-
   pname = "babble-trainer";
-  version = "0.0.0";
+  version = "1.3.8";
   pyproject = false;
   doCheck = false;
 
-  # https://github.com/Naraenda/BabbleTrainer/commits/various-fixes/
-  #
-  # TODO: move this over to main once this gets merged:
-  #   https://github.com/Project-Babble/BabbleTrainer/pull/4
-  src = pkgs.fetchFromGitHub {
-    owner = "Naraenda";
-    repo = "BabbleTrainer";
-    rev = "5c0da3da660e4f70f9d6c8ea00a02c57b4e3d170";
-    hash = "sha256-o2WFfhQHxsdK3xbTQYN2z+jOQqUJeJ1YTVnfOqUA5Hk=";
-  };
+  src = babble-trainer-src;
 
   # The original project uses pyinstaller with a .spec-file.
   # We don't really need that because we can ensure that the
   # right depedencies are installed ourselves. We instead add
   # a shebang pointing to our own python environment!
   #
-  # If BabbleTrainer every becomes more complex, we may need to
+  # TODO If BabbleTrainer every becomes more complex, we may need to
   # to redirect from /bin to /lib to not pollute /bin!
   buildPhase = ''
     mkdir -p $out/bin
     echo "#!${pythonEnv}/bin/python3" > $out/bin/babble-trainer
-    cat trainermin.py >> $out/bin/babble-trainer
+    cat main.py >> $out/bin/babble-trainer
     chmod +x $out/bin/babble-trainer
+
+    cp trainer_distsampler.py $out/bin/
+    cp models.py $out/bin/
+    cp data.py $out/bin/
   '';
 
   installPhase = ''
     # Skipped!
   '';
 
+  patches = [
+    ./tmp-dir-fixes.patch
+  ];
+
   meta = {
-    description = "BabbleTrainer built with PyInstaller";
+    description = "BabbleTrainer";
     platforms = pkgs.lib.platforms.linux;
-    maintainers = with lib.maintainers; [ naraenda ];
+    maintainers = with lib.maintainers; [ toasteruwu ];
   };
 }
